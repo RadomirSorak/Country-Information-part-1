d729f9bef5b988bc8fd87b15b620addb35b11ec0
Radomir Sorak <radomirsorak@MacBook-Pro-van-Radomir.local>
2022-02-09 23:49:35 +0100

    Countries

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
-console.log('Hallo daar!');
+import axios from "axios";
+
+async function getCountries() {
+
+    try {
+        const result = await axios.get("https://restcountries.com/v2/all");
+        console.log(result.data);
+
+        result.data.sort((a, b) => {
+            return a.population - b.population;
+        })
+        getAllCountries(result.data);
+    }
+    catch(e) {
+        console.error(e);
+    }
+}
+
+getCountries();
+
+function getAllCountries(countries){
+    const unorderedCountryList = document.getElementById("country-list");
+
+    countries.map((allCOuntries) => {
+
+        const countryList = document.createElement("li");
+
+        countryList.innerHTML = `
+        <img src="${allCOuntries.flag}" class="flag"/>
+        <h3>${allCOuntries.name}</h3>
+        <p>Has population of ${allCOuntries.population} poeple</p> `
+
+        unorderedCountryList.appendChild(countryList);
+
+    })
+}
+
+
+
